89b3b8bdbd16e54b5f9c8fac56c13bd015ad749f
ueki <ueki@yudo.jp>
2015-03-06 21:51:04 +0900

    create project

---
+++ OvercoatExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OvercoatExample
+//
+//  Created by yuji ueki on 2015/03/06.
+//  Copyright (c) 2015年 yudo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OvercoatExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OvercoatExample
+//
+//  Created by yuji ueki on 2015/03/06.
+//  Copyright (c) 2015年 yudo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
