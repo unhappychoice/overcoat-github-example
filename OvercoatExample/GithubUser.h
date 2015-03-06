5264801453feba12c3886a02c005454d30e11cd8
ueki <ueki@yudo.jp>
2015-03-06 22:04:51 +0900

    add github user class

---
+++ OvercoatExample/GithubUser.h
@@ -0,0 +1,13 @@
+//
+//  GithubUser.h
+//  OvercoatExample
+//
+//  Created by yuji ueki on 2015/03/06.
+//  Copyright (c) 2015年 yudo. All rights reserved.
+//
+
+#import "MTLModel.h"
+
+@interface GithubUser : MTLModel
+
+@end
---
+++ OvercoatExample/GithubUser.m
@@ -0,0 +1,13 @@
+//
+//  GithubUser.m
+//  OvercoatExample
+//
+//  Created by yuji ueki on 2015/03/06.
+//  Copyright (c) 2015年 yudo. All rights reserved.
+//
+
+#import "GithubUser.h"
+
+@implementation GithubUser
+
+@end
