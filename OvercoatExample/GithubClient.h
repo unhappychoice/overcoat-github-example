c31e082e4dc8f7972c187fe360c0bcfe6a6323d7
ueki <ueki@yudo.jp>
2015-03-06 22:00:11 +0900

    add github client class

---
+++ OvercoatExample/GithubClient.h
@@ -0,0 +1,13 @@
+//
+//  GithubClient.h
+//  OvercoatExample
+//
+//  Created by yuji ueki on 2015/03/06.
+//  Copyright (c) 2015年 yudo. All rights reserved.
+//
+
+#import "OVCHTTPSessionManager.h"
+
+@interface GithubClient : OVCHTTPSessionManager
+
+@end
