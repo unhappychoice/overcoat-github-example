e7755641c6f8d97daa27fe793733865a034c96b7
ueki <ueki@yudo.jp>
2015-03-09 01:42:36 +0900

    add user view controller

---
+++ OvercoatExample/UserViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserViewController.h
+//  OvercoatExample
+//
+//  Created by yuji ueki on 2015/03/09.
+//  Copyright (c) 2015年 yudo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserViewController : UIViewController
+
+@end
---
+++ OvercoatExample/UserViewController.m
@@ -0,0 +1,37 @@
+//
+//  UserViewController.m
+//  OvercoatExample
+//
+//  Created by yuji ueki on 2015/03/09.
+//  Copyright (c) 2015年 yudo. All rights reserved.
+//
+
+#import "UserViewController.h"
+
+@interface UserViewController ()
+
+@end
+
+@implementation UserViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
